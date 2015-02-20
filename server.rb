49f7951c05acd96eac5531e64be10d3e147d72a9
Umer Dar <umerbashirdar@gmail.com>
2015-02-19 22:30:30 -0500

    serious server work

--- server.rb
+++ server.rb
@@ -12,6 +12,21 @@ require 'active_record'
 
 enable(:sessions)
 
+post '/sessions' do
+# binding.pry
+  user = User.find_by(email: params[:email])
+
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/users/#{user.id}"
+  else
+    redirect "/"
+  end
+end
+
+
+
+
 get ('/users/:user_id/categories/:category_id') do
   binding.pry
 
@@ -35,14 +50,14 @@ new_user = User.create({name: params["name"], email: params["email"], password: 
 redirect "users/#{new_user.id}"
 end
 
-get ('/users') do
-end
+# get ('/users') do
+# end
 
-
-get ('/users/:id/activities/:activity_id') do
+get ('/users/:id/activities') do
   # if session[:user_id] != nil
 @user = User.find(params["id"])
- render(:erb, :user) #you have to render it so it can be read. In rails, that's automatic. talk about how the @ sign in rails makes it so you don't have to render, but maybe you also have to render user: user if you had "user" instead of "@user". What's the difference????
+# @activity = Activity.find(params["activity_id"])
+ render(:erb, :user_activities) #you have to render it so it can be read. In rails, that's automatic. talk about how the @ sign in rails makes it so you don't have to render, but maybe you also have to render user: user if you had "user" instead of "@user". What's the difference????
 
   # else
   #    redirect_to '/'
@@ -50,14 +65,32 @@ get ('/users/:id/activities/:activity_id') do
 end
 
 get '/users/:id' do
+@user = User.find(params["id"])
+ render(:erb, :users)
 # 1
 # 2
 # 3
 end
 
-get 'users/:id/categories' do
+get '/categories' do
+
+@category = Category.all
+# binding.pry
+render(:erb, :categories)
 
 end
+
+post '/categoryredirect' do
+# @category = Category.find_by(params["id"])
+
+redirect "/categories/#{params['category-id']}"
+end
+
+get '/categories/:id' do
+
+end
+
+
 
 
 
